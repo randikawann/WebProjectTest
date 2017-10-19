97cb2bc581796f499a23d182a742bc7c370b701a
randikawann <randikawann@gmail.com>
2017-10-19 16:35:26 -0400

    this is first commit

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+﻿function display() {
+    var myValue = document.getElementById('myTextBox').value;
+    if (myValue.length == 0) {
+        alert('enter real value');
+        return;
+    }
+    var myTitle = document.getElementById('headId');
+    myTitle.innerHTML = myValue;
+}
